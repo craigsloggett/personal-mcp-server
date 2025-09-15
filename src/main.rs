b5ff3177a11300abc33d1f6c38abab285e06894d
Craig Sloggett <craigsloggett@gmail.com>
2025-09-15 16:40:19 -0400

    chore: Add example from the Rust book

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
-use std::cmp::Ordering;
-use std::io;
+use personal_mcp_server::{search, search_case_insensitive};
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
 
 fn main() {
-    println!("Guess the number!");
+    let args: Vec<String> = env::args().collect();
 
-    let secret_number: u32 = 42;
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
 }
